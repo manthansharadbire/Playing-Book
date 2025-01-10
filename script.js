55e21bcdd0e303051f50cebd0e67fdb2337ca93e
manthansharadbire <manthansharadbire@gmail.com>
2025-01-10 21:28:13 +0530

    Script provided

--- script.js
+++ script.js
@@ -7,9 +7,26 @@ const players = [
     "Yuvraj Singh",
     "Brendon McCullum"
 ]
-
+function loadPlayers(){
 const playersContainer = document.getElementById('players-container');
+playersContainer.innerHTML = "";
 
 for(let i=0; i<players.length; i++){
     playersContainer.innerHTML += `<div class='player-card'>${i+1}) ${players[i]}</div>`
+}
+}
+loadPlayers()
+
+const playerInput = document.getElementById('input-name');
+function addPlayer(){
+    players.push(playerInput.value);
+    playerInput.value = "";
+    loadPlayers();
+}
+
+function removePlayer(){
+    const playerIndex = players.indexOf(playerInput.value);
+    players.splice(playerIndex, 1);
+    playerInput.value = "";
+    loadPlayers();
 }
